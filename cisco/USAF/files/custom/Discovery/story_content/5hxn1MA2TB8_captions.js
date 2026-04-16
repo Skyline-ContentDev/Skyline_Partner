6686ac15881a9bee48d7c20b1b6940987b80cd6c
JayWhale <dev+github@skyline-ats.com>
2026-04-17 07:46:06 +1000

    update

---
+++ cisco/USAF/files/custom/Discovery/story_content/5hxn1MA2TB8_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5hxn1MA2TB8_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.101.34925.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:03.362%0D%0ANext,%20you%20will%20select%20the%20device%20and%20then%20Actions.%0D%0A%0D%0A"}]})
---
+++ cisco/USAF/files/custom/SWIM/story_content/63KRiFI4N0n_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/63KRiFI4N0n_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.101.34925.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:02.619%0D%0AProvide%20a%20task%20name,%20and%20then%20select%20next.%0D%0A%0D%0A"}]})
